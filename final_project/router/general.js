f82a7a93d2d33f169ac447b1c67878565d1e4e3c
colereec <ReeceDavidCole@gmail.com>
2026-01-30 02:59:49 -0500

    Task 3 completed.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,8 +32,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //TASK 3
+  const author = req.params.author;
+  const booksWithAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+  if (booksWithAuthor.length){
+    return res.status(200).send(JSON.stringify(booksWithAuthor));
+  }
+  else{
+    return res.status(404).json({message: "Author not found"});
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
